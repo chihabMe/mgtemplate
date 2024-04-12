e949754df249068eff53342b8ef7eda54228b586
chihabMe <chihab.mg.me@gmail.com>
2024-04-12 23:57:16 +0100

    added schema validation in signup route

--- src/app/api/accounts/signup/route.ts
+++ src/app/api/accounts/signup/route.ts
@@ -1,10 +1,29 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { BAD_REQUEST, CREATED } from "http-status";
+import { NextApiRequest } from "next";
+import { NextResponse } from "next/server";
+import * as z from "zod"
 
-
-export default  async function  (req: NextApiRequest, res: NextApiResponse)  {
-    const {email,password} = req.body
-    if(!email || !password)return res.status(403).json({message:"invalid fields"})
+const schema = z.object({
+    email: z.string().email(),
+    password: z.string()
+})
+export async function POST(req: NextApiRequest) {
+    const valid = schema.safeParse(req.body)
+    if (!valid.success) {
+        return NextResponse.json({
+            message: "invalid fields",
+            errors: valid.error.formErrors.fieldErrors
+        }, {
+            status: BAD_REQUEST
+        })
+    }
+    const { email, password } = req.body
+    if (!email || !password) return NextResponse.json({ message: "invalid fields" }, {
+        status: BAD_REQUEST
+    })
     console.log(email)
     console.log(password)
-    res.status(201).json({ message: "registred!" })
+    return NextResponse.json({ message: "registred!" }, {
+        status: CREATED
+    })
 }
