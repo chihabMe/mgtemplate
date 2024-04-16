c99c9a305490585d3a9cf2e7200b9913fe69dc83
chihabMe <chihab.mg.me@gmail.com>
2024-04-16 19:55:55 +0100

    improved the cretion of a prisma client

--- prisma/migrations/20240413152102_imrpve_the_user_model/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `User` ADD COLUMN `active` BOOLEAN NOT NULL DEFAULT true,
-    ADD COLUMN `verified` BOOLEAN NOT NULL DEFAULT false;
---
+++ prisma/migrations/20240416180303_init/migration.sql
@@ -0,0 +1,24 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "active" BOOLEAN NOT NULL DEFAULT false,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_username_idx" ON "User"("username");
